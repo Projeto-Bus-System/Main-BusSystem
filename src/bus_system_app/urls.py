0295b87a1500bf3cae4b2ff37befbbae5dd9a08a
ORuan <ruanpablomp@gmail.com>
2020-06-18 03:58:43 -0300

    remove directory frontend; migrate files of template for project real; create structure basic

---
+++ src/bus_system_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+
+urlpatterns = [
+    #path('', index),
+    #path('cadastro/', cadastro_user, name="CadastroDeUsuario")
+    #path('criar/', criar),
+    #path('detalhe/<int:id>/', detalhes),
+    #path('editar/<int:id>/', editar)
+]
