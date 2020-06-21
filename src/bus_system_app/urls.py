70aedfdf2244cf9791167a3bd96db434451cfeaf
ORuan <ruanpablomp@gmail.com>
2020-06-20 21:41:48 -0300

    add login template

--- src/bus_system_app/urls.py
+++ src/bus_system_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import cadastro_user
+from .views import cadastro_user, login_user
 
 
 urlpatterns = [
     #path('', index),
-    path('cadastro/', cadastro_user, name="CadastroDeUsuario")
+    path('cadastro/', cadastro_user, name="CadastroUsuario"),
+    path('login/', login_user, name="LoginUsuario")
     #path('criar/', criar),
     #path('detalhe/<int:id>/', detalhes),
     #path('editar/<int:id>/', editar)
--- src/bus_system_app/views.py
+++ src/bus_system_app/views.py
@@ -5,3 +5,8 @@ from django.shortcuts import render, HttpResponse
 def cadastro_user(request):
     
     return render(request, 'cadastro/cadastro.html',{})
+
+
+def login_user(request):
+    
+    return render(request, 'login/login.html',{})
