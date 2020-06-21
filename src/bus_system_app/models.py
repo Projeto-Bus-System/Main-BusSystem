cc6c8c02c16db4bcb7ea5180f94e906a56243d84
ORuan <ruanpablomp@gmail.com>
2020-06-21 17:08:02 -0300

    models initialized

--- src/bus_system_app/models.py
+++ src/bus_system_app/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Aluno(models.Model):
+    
+    #matricula, ponto, telefone_responsavel, telefone
+    matricula = models.CharField(max_length=20, null=False, blank=False)
