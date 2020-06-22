3d857bfd323f14e457b62a663375b9f900d20947
ORuan <ruanpablomp@gmail.com>
2020-06-22 00:48:01 -0300

    add modals of users

--- src/bus_system_app/models.py
+++ src/bus_system_app/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Aluno(models.Model):
+class Aluno(User):
     
-    #matricula, ponto, telefone_responsavel, telefone
-    matricula = models.CharField(max_length=20, null=False, blank=False)
+    telefone_responsavel = models.IntegerField(null=False, blank=False)
+    telefone = models.IntegerField(null=False, blank=False)
+    ponto = models.CharField(max_length=500, null=False, blank=False)#Os pontos que o aluno geralmente pega (2)
+    matricula = models.CharField(max_length=40, null=False, blank=False)
+
+class Motorista(User):
+    Onibus = models.CharField(max_length=20, null=False, blank=False)
+    pontos = models.CharField(max_length=200, null=False, blank=False) #Todos os pontos que o motorista passa(all)
+    passou_no_ponto = models.BooleanField(null=False,  default=False)
+
+class Admin(User):
+    nome_admin = models.CharField(max_length=40, null=False, blank=False)
--- src/bus_system_app/views.py
+++ src/bus_system_app/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render, HttpResponse
-
+from django.shortcuts import render, HttpResponse, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 def cadastro_user(request):
+    if request.method == 'POST':
+        form_usuario = UserCreationForm(request.POST)
+        if form_usuario.is_valid():
+            form_usuario.save()
+            return redirect('cadastro_user')
+    else:
+        form_usuario = UserCreationForm()
+
+    return render(request, 'cadastro/cadastro.html', {"form_usuario":form_usuario })
+
+
+
+
+#def cadastro_user(request):
     
-    return render(request, 'cadastro/cadastro.html',{})
+#    return render(request, 'cadastro/cadastro.html',{})
 
 
 def login_user(request):
