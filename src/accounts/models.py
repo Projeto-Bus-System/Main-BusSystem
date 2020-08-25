7230ea11d84ed7eeda794108baa247b233f03629
ORuan <ruanpablomp@gmail.com>
2020-08-25 19:51:51 -0300

    create table driver, and student

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -3,9 +3,14 @@ from django.db import models
 
 
 class Student(models.Model):
-    matricula = models.Charfield(max_length=11, null=False, blank=False)
-    """ponto,
-    telefone,
-    telefone_responsavel"""
+    matricula = models.CharField(max_length=11, null=False, blank=False)
+    ponto = models.CharField(max_length=150, null=False, blank=False),
+    telefone = models.CharField(max_length=12, null=False, blank=False),
+    telefone_responsavel = models.CharField(max_length=12, null=False, blank=False),
 
 
+
+class Driver(models.Model):
+    onibus = models.CharField(max_length=11, null=False, blank=False)
+    pontos =  models.CharField(max_length=1000, null=False, blank=False),
+    possou_ponto = models.BooleanField(default=False),
