e905936ecce709dd3c316a74acdc964b06f66f75
ORuan <ruanpablomp@gmail.com>
2020-09-03 20:08:27 -0300

    add new project with gitignore

--- src/map/models.py
+++ src/map/models.py
@@ -1,1 +1,15 @@
 from django.db import models
+#from driver.models import Driver
+
+class Bookmarks(models.Model):
+    lat = models.CharField(max_length=15, null=False, blank=False)
+    long = models.CharField(max_length=15, null=False, blank=False)
+    status_driver = models.BooleanField(default=False)
+    name_point = models.CharField(max_length=10, null=False, blank=False)
+
+
+"""
+class Map(models.Model):
+    driver = models.OneToOneField(Driver, on_delete=models.CASCADE)
+    bookmarks = models.ForeignKey(Bookmarks, on_delete=models.CASCADE)  
+"""
